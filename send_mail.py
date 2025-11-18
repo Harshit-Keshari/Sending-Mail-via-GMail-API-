5803c54379eaa4b44ab8b7fff9ba5a5f261f33bf
Harshit Keshari <harshitkeshari9621@gmail.com>
2025-11-19 01:54:23 +0530

    sdf

--- send_mail.py
+++ send_mail.py
@@ -45,14 +45,14 @@ def aunthentication():
             # If there are no valid credentials available, let the user log in.
             flow = InstalledAppFlow.from_client_secrets_file(
                 'client_secrets.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=8080)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
 
 
-def prepare_and_send_email(sender, recipient, subject, message_text, im0: bytes):
+def prepare_and_send_email(sender, recipient, subject, message_text, im0= None):
     """Prepares and send email with attachment to the participants 
 
     Args:
@@ -82,86 +82,53 @@ def prepare_and_send_email(sender, recipient, subject, message_text, im0: bytes)
         print(f'An error occurred: {error}')
 
 
-def create_message(sender, to, subject, message_text, img_file):
-    """Create a message for an email.
+def create_message(sender, to, subject, message_text, img_file=None):
+    """Create a message for an email."""
 
-    Args:
-        sender: Email address of the sender.
-        to: Email address of the receiver.
-        subject: The subject of the email message.
-        message_text: The text of the email message.
-        img_file: The image to be attached
+    # ---------------------------------------
+    # CASE 1: No image provided -> send text only
+    # ---------------------------------------
+    if img_file is None:
+        message = MIMEText(message_text)
+        message['to'] = to
+        message['from'] = sender
+        message['subject'] = subject
 
-    Returns:
-        An object containing a base64url encoded email object.
-    """
-    # create a multipart email message with attachment
+        return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+    # ---------------------------------------
+    # CASE 2: Image exists -> send attachment
+    # ---------------------------------------
     message = MIMEMultipart()
-
     message['from'] = sender
     message['to'] = to
     message['subject'] = subject
 
-    #create a directory to store the images that are attached to the email
+    # create directory
     base_loc = 'static/violations/'
-    location = 'GRIL Office'
-    
-    #get current date and time
-    current_date_time = time.time()
-    formatted_date_time = time.strftime("%H:%M:%S_%d-%m-%Y", time.localtime(current_date_time))
-    
-    #if base_loc doesn't exist, create it
     if not os.path.exists(base_loc):
         os.makedirs(base_loc)
-    
-    file_name = base_loc+'violation_'+location + '_' + formatted_date_time + '.jpg'
 
-    #convert img_file into jpeg format and save it in the file_name
+    location = 'GRIL Office'
+    current_date_time = time.time()
+    formatted_date_time = time.strftime("%H:%M:%S_%d-%m-%Y", time.localtime(current_date_time))
+    file_name = base_loc + 'violation_' + location + '_' + formatted_date_time + '.jpg'
+
+    # save image
     cv2.imencode('.jpg', img_file)[1].tofile(file_name)
 
-    msg = MIMEText(message_text)
-    message.attach(msg)
+    # attach text
+    message.attach(MIMEText(message_text))
 
-    content_type, encoding = mimetypes.guess_type(file_name)
-    main_type, sub_type = content_type.split('/', 1)
+    # attach image
+    with open(file_name, 'rb') as f:
+        img_data = f.read()
+        img = MIMEImage(img_data)
+        img.add_header('Content-Disposition', 'attachment', filename=os.path.basename(file_name))
+        message.attach(img)
 
-    print(f'Attachment main_type = {main_type}, subtype= {sub_type}, and encoding = {encoding}')
-
-    # code to attach text, image, pdf and other files
-    # if attachment is a text file
-    if main_type == 'text':
-        fp = open(file_name, 'rb')
-        msg = MIMEText(fp.read(), _subtype=sub_type)
-        fp.close()
-    # if attachment is an image file    
-    elif main_type == 'image':
-        fp = open(file_name, 'rb')
-        msg = MIMEImage(fp.read(), _subtype=sub_type)
-        fp.close()
-    # if attachment is a pdf file, then we need to set the main_type to application and sub_type to octet-stream
-    elif main_type == 'application' and sub_type=='pdf' and encoding == None: #Reference - https://coderzcolumn.com/tutorials/python/mimetypes-guide-to-determine-mime-type-of-file
-        print("INSIDE PDF")
-        main_type = 'application'
-        sub_type = 'octet-stream'
-        fp = open(file_name, 'rb')
-        msg = MIMEBase(main_type, sub_type)
-        msg.set_payload(fp.read())
-        encoders.encode_base64(msg)
-        fp.close()
-    #if attachment is anything else
-    else:
-        fp = open(file_name, 'rb')
-        msg = MIMEBase(main_type, sub_type)
-        msg.set_payload(fp.read())
-        fp.close()
-    
-    filename = os.path.basename(file_name)
-    # add attachment to the message header
-    msg.add_header('Content-Disposition', 'attachment', filename=filename)
-    message.attach(msg)
-
-    #convert the message into a string
     return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
 
 
 def send_message(service, user_id, message):
@@ -190,10 +157,10 @@ if __name__ == '__main__':
     # Uncomment the following lines to run the code locally
     # set sender and recipient accordingly
     # sender must be a gmail account using which you have enabled the gmail API
-    prepare_and_send_email(sender='', 
-                           recipient='',
-                           subject= '', 
-                           message_text= ''
+    prepare_and_send_email(sender='harshitkeshari9621@gmail.com', 
+                           recipient='har1998shit@gmail.com',
+                           subject= 'Hi Automated Mail', 
+                           message_text= 'Hi How Are You ??'
     )
     pass
     
